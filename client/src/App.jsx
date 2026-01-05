d6a545e65f189e1ef0077302b5a61cf5eb6e2783
unknown <sadaksamo>
2026-01-05 15:04:53 +0300

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Home from './Pages/Home'
+import Auth from './Pages/Auth'
+import Dashboard from './Pages/Dashboard'
+import Projects from './Pages/Projects'
+import About from './Pages/About'
+
+const App = () => {
+  return (
+    <div>
+      
+
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/Auth' element={<Auth/>}/>
+        <Route path='/dashboard' element={<Dashboard/>}/>
+        <Route path='/projects' element={<Projects/>}/>
+        <Route path='/about' element={<About/>}/>
+      </Routes>
+    </div>
+  )
+}
+
+export default App
