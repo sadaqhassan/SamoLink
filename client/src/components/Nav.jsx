97e7fff3ef1bf69b9c5d415226b872df3d6186a1
sadaksamo <sadaksamor99@gmail.com>
2026-01-05 16:31:02 +0300

    Completed nav section

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -1,25 +1,48 @@
 import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
-import{ MoonIcon, Sun } from 'lucide-react'
+import{ MenuIcon, MoonIcon, Sun } from 'lucide-react'
 
 const Nav = () => {
     const [mode,setMode] = useState('light')
+    const [menuOpen,setMenuOpen] = useState(false)
   return (
     <main>
       {/* //deskttop/ */}
-      <div className='md:flex hidden justify-between px-4 py-2'>
+      <div className={`md:flex hidden justify-between ${mode === 'light' ? 'bg-gray-700 px-2 py-2 text-white' : 'bg-white px-2 py-2 text-black'} px-4 py-2 border-b-1 shadow-2xl`}>
         <p>SamoLink</p>
         <div className='flex space-x-3'>
             <Link to={'/'}>Home</Link>
             <Link to={'/about'}>About</Link>
             <Link to={'/projects'}>Projects</Link>
         </div>
-        <div>
-            <button>{mode === 'light' ? <MoonIcon/> : <Sun/>}</button>
-            <button className='px-3 py-1 rounded bg-green-600 '>Login</button>
+        <div className='flex space-x-4'>
+            <button onClick={()=>{mode === 'dark' ? setMode('light') : setMode('dark')}} >{mode === 'light' ? <MoonIcon/> : <Sun/>}</button>
+            <button className='px-3 py-1 rounded bg-green-600 text-white'>Login</button>
         </div>
       </div>
 
+
+      {/* //mobile */}
+      
+      <div className={`flex md:hidden justify-between ${mode === 'light' ? 'bg-gray-700 text-white px-2 py-2' : 'bg-white text-black px-2 py-2'} px-4 py-2 border-b-1 shadow-2xl`}>
+        <p>SamoLink</p>
+       
+        <div className='flex space-x-4'>
+            <button onClick={()=>setMenuOpen(!menuOpen)}><MenuIcon/></button>
+            <button onClick={()=>{mode === 'dark' ? setMode('light') : setMode('dark')}} >{mode === 'dark' ? <MoonIcon/> : <Sun/>}</button>
+        </div>
+      </div>
+
+{
+    menuOpen &&
+     <div className={`flex  md:hidden flex-col space-y-3 pl-5 py-4 ${mode === 'light' ? ' bg-gray-600 text-white' : ' bg-white text-black'}`}>
+        <Link to={'/'}>Home</Link>
+        <Link to={'/about'}>About</Link>
+        <Link to={'/projects'}>Projects</Link>
+        <button className='px-3 py-1 rounded bg-green-600 text-white'>Login</button>
+
+    </div>
+}
 
     </main>
   )
