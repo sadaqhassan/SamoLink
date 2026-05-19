12741f568802a1740beea85fc74548947dccdb76
sadaqhassan <abdallaabdurahman845@gmail.com>
2026-05-19 10:22:27 +0300

    register and login controller

---
+++ server/Controllers/user.controller.js
@@ -0,0 +1,64 @@
+import bcrypt from 'bcryptjs'
+import { User } from '../Model/user.schema.js';
+import jwt from 'jsonwebtoken'
+//register
+
+
+export const registerApi = async (req,res)=>{
+    const {email,password,role,name} = req.body
+
+    try {
+        const user = await User.findOne({email});
+
+        if(user){
+            return res.status(400).json({success:false,message:"this user is alrady exist"})
+        }
+
+        let role ;
+
+        if(email === process.env.email){
+            role = "admin"
+        }
+
+        const hashPassword = await bcrypt.hash(password,10);
+
+        const newUser =  new User({
+            name,
+            email,
+            password:hashPassword,
+            role
+        });
+
+        await newUser.save();
+
+        return res.status(201).json({success:true ,message:"register successfully"});
+
+    } catch (error) {
+        res.status(500).json({success:false,message:"server error"})
+        console.log(error)
+    }
+}
+
+
+//login
+
+export const loginApi = async (req,res)=>{
+    const {email,password} = req.body
+    try{
+        const user = await User.findOne({email});
+        if(!user){
+            return res.status(401).json({success:false,message:"invalid credentials"})
+        }
+        const isCompare = await bcrypt.compare(password,user.password);
+
+        if(!isCompare){
+            return res.status(401).json({success:false,message:"invalid credentials"})
+        }
+        const token = jwt.sign({id:user._id,role:user.role},process.env.JWT_SECRET,{expiresIn:"1d"});
+
+        res.cookie("token",token).status(200).json({success:true,message:"welcome back"})
+    }catch(error){
+        res.status(500).json({success:false,message:"server error"})
+        console.log(error)
+    }
+}
