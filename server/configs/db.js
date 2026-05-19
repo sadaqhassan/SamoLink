ab29f6a26ce545fee52167a102c2f12398606c51
sadaqhassan <abdallaabdurahman845@gmail.com>
2026-05-19 10:22:15 +0300

    dbjs

---
+++ server/configs/db.js
@@ -0,0 +1,9 @@
+import mongoose from "mongoose"
+
+const connectiondb = async ()=>{
+    const url = process.env.DB_URL
+    mongoose.connect(url).then(()=>console.log("connected to the db")).catch((err)=>console.log(err));
+};
+
+export default connectiondb;
+
