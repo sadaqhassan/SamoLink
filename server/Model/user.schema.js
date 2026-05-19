dda75cdeb1b29d0b25187c296e9858e7ee9d9b88
sadaqhassan <abdallaabdurahman845@gmail.com>
2026-05-19 10:22:34 +0300

    user schema

---
+++ server/Model/user.schema.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+
+const userSchema = mongoose.Schema({
+    name:{
+        type:String,
+        required:true
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    password:{
+        type:String,
+        required:true
+    },
+    role:{
+        type:String,
+        required:true,
+        enum: ["user","admin"],
+        default: "user"
+    },
+},{timeStamps:true})
+
+export const User = mongoose.model("user",userSchema);
