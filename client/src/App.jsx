88c6c233e00e5ee3e9a4d0341a04f38412daba03
sadaqhassan <abdallaabdurahman845@gmail.com>
2026-05-20 14:08:30 +0300

    paths

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,14 +3,15 @@ import { Route, Routes } from 'react-router-dom'
 import Login from './Pages/Login'
 import {Toaster} from 'react-hot-toast'
 import { useUser } from './contexts/AuthContexts'
-import Feeds from './Pages/Feeds'
+import Layout from './Pages/Layout'
+import CreatePost from './Pages/CreatePost'
 const App = () => {
   const {user} = useUser();
   return (
     <div>
       {<Toaster/>}
       <Routes>
-        <Route path='/' element={user != null  ? <Feeds/> :  <Login/>}>
+        <Route path='/' element={user != null  ? <Layout/> :  <Login/>}>
         </Route>
       </Routes>
     </div>
