f088a1de148a6851a27b6214fef575fce014ad6c
sadaqhassan <abdallaabdurahman845@gmail.com>
2026-05-20 15:28:15 +0300

    usr api

--- server/Routes/user.route.js
+++ server/Routes/user.route.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
-import { loginApi, registerApi } from "../Controllers/user.controller.js";
+import { getUser, loginApi, registerApi } from "../Controllers/user.controller.js";
+import { isAuth } from "../middlewares/isAuth.js";
 
 const user = Router();
 
 user.post('/register',registerApi);
 user.post('/login',loginApi);
+user.get('/get-user',isAuth,getUser);
 
 export const userRoute = user;
