3e997da4c49be77c60050f358f059dbc734c077b
sadaqhassan <abdallaabdurahman845@gmail.com>
2026-05-20 15:27:36 +0300

    right

--- client/src/components/Right.jsx
+++ client/src/components/Right.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Right = () => {
   return (
-    <div className='md:flex flex-col hidden w-[280px] bg-light z-10'>Right</div>
+    <div className='md:flex hidden w-[280px] sticky top-0 z-10 h-screen'>
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRTKIAC9UPYGMqLOKnCzFxva3YYTW_rNegvKw&s" alt="" />
+    </div>
   )
 }
 
