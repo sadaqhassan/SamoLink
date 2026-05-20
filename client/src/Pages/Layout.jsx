2e5a576aae5206a43491c69ebde328742c3f6c2f
sadaqhassan <abdallaabdurahman845@gmail.com>
2026-05-20 15:27:54 +0300

    layouts

--- client/src/Pages/Layout.jsx
+++ client/src/Pages/Layout.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Side from '../components/Side'
 import Feeds from '../components/Feeds'
 import Right from '../components/Right'
+import toast from'react-hot-toast'
+import { useUser} from '../contexts/AuthContexts'
+import { Outlet } from 'react-router-dom'
 
 const Layout = () => {
+  const { userApi , user} = useUser();
+
   return (
     <div className='flex justify-between'>
         <Side/>
