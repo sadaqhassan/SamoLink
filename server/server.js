39575352b64a342666ffcb96dfffc3f346604f06
sadaqhassan <abdallaabdurahman845@gmail.com>
2026-05-23 12:58:58 +0300

    sercver

--- server/server.js
+++ server/server.js
@@ -2,11 +2,13 @@ import express from 'express'
 import configApp from './configs/config.js';
 import connectiondb from './configs/db.js';
 import { userRoute } from './Routes/user.route.js';
+import { postRoute } from './Routes/posts.route.js';
 
 const app = express();
 
 configApp(app);
 
 app.use('/api/user',userRoute);
+app.use('/api/posts',postRoute);
 
 connectiondb()
