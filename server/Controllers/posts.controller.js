6530a01318eac552cffb6f33bf7bfa053b170a3c
sadaqhassan <abdallaabdurahman845@gmail.com>
2026-05-23 12:59:03 +0300

    posts

---
+++ server/Controllers/posts.controller.js
@@ -0,0 +1,39 @@
+import postsSchema from "../Model/posts.schema.js"
+import { User } from "../Model/user.schema.js"
+
+//post 
+export const postApi  = async (req,res) => {
+    const {userId} = req
+    const {content,image} = req.body
+    if(!userId) return res.status(400).json({success:false,message:"you can't post "})
+        
+        if(!content || !image){
+            return res.status(400).json({success:false,message:"fill all field"})
+        }
+    try {
+        const user = await User.findById(userId);
+        
+        const creating = await postsSchema.create({
+            content,image,userId,owner:user.name
+        });
+        return  res.status(201).json({success:true ,message:"successfully posted"});
+
+    } catch (error) {
+        res.status(500).json({success:false,message:"server error"})
+        console.log(error)
+    }
+}
+
+
+//get
+export const getPostsApi = async (req,res) => {
+    try {
+        const posts = await postsSchema.find();
+        res.status(201).json({success:true,data:posts})
+    } catch (error) {
+        res.status(500).json({success:false,message:"server error"})
+        console.log(error)
+    }
+};
+
+
