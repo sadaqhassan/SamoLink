173f0993513c6a2feaccd947f995b0ba65f3a119
sadaqhassan <abdallaabdurahman845@gmail.com>
2026-05-23 19:27:06 +0300

    update api

--- server/Routes/user.route.js
+++ server/Routes/user.route.js
@@ -7,5 +7,6 @@ const user = Router();
 user.post('/register',registerApi);
 user.post('/login',loginApi);
 user.get('/get-user',isAuth,getUser);
+user.put('/update',isAuth,updateProfile);
 
 export const userRoute = user;
