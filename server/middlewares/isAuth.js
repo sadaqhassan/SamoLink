ffad950d978eb5151c4d2a99b6a392425d4e6270
sadaqhassan <abdallaabdurahman845@gmail.com>
2026-05-23 12:59:13 +0300

    check auth

--- server/middlewares/isAuth.js
+++ server/middlewares/isAuth.js
@@ -1,4 +1,5 @@
 import jwt from 'jsonwebtoken'
+import { User } from '../Model/user.schema.js';
 
 export const isAuth = async (req,res,next) => {
   const token = req.cookies.token
@@ -12,8 +13,13 @@ export const isAuth = async (req,res,next) => {
   if(!verify){
     return res.status(403).json({success:false ,message:"please are not authed"})
   }
+  
+  const user =   await User.findById(verify.id);
+  if(!user){
+    return res.status(404).json({success:false,message:"user not found"})
+  }
 
-  req.userId = verify.id;
+  req.userId = verify.id
 
   next();
 }
