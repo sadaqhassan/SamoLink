2294d3b129f8c69c69be952d65905113f2625196
sadaqhassan <abdallaabdurahman845@gmail.com>
2026-05-23 12:59:21 +0300

    post schema

---
+++ server/Model/posts.schema.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+
+const postSchema = mongoose.Schema({
+    userId:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"user",
+        required:true
+    },
+    content:{
+        type:String,
+        required:true
+    },
+    image:{
+        type:String,
+    },
+    owner:{
+        type:String,
+    },
+},{
+    timestamps:true
+});
+
+export default mongoose.model("Post",postSchema)
