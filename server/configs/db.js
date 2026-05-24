a208821819a4e0daf561fe217d0153b6ef79988b
sadaqhassan <abdallaabdurahman845@gmail.com>
2026-05-24 22:32:31 +0300

    some fixed error

--- server/configs/db.js
+++ server/configs/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose"
 
 const connectiondb = async ()=>{
     const url = process.env.DB_URL
-    mongoose.connect(url).then(()=>console.log("connected to the db")).catch((err)=>console.log(err));
+    await mongoose.connect(url).then(()=>console.log("connected to the db")).catch((err)=>console.log(err));
 };
 
 export default connectiondb;
